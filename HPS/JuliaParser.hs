1781a09f2d7ec58cfe700ad18aac900855923708
Istvan Burbank <ipb7@cornell.edu>
2017-05-04 14:59:08 -0400

    Julia Parser
    Split into its own file, seems to work

---
+++ HPS/JuliaParser.hs
@@ -0,0 +1,38 @@
+module JuliaParser(Exp, juliaParse) where
+
+import Text.Parsec
+import Text.Parsec.Expr
+import Text.Parsec.Combinator
+import Data.Functor
+
+-- Taken From https://gist.github.com/m00nlight/2868363ec217f97072b4
+
+data Exp = Num Int
+         | Var String
+         | Add Exp Exp
+         | Sub Exp Exp
+         | Mul Exp Exp
+         | Div Exp Exp
+         | Pow Exp Exp
+         | Pos Exp
+         | Neg Exp
+           deriving (Show)
+
+--------------------- PARSER ---------------------
+expr = buildExpressionParser table factor <?> "expr"
+
+--table :: OperatorTable s u m Exp
+table = [[prefix "-" (Neg), prefix "+" (Pos)]
+        ,[op "^" (Pow) AssocRight]
+        ,[op "*" (Mul) AssocRight, op "/" (Div) AssocRight]
+        ,[op "+" (Add) AssocLeft, op "-" (Sub) AssocLeft]]
+        where op s f assoc = Infix (f <$ string s) assoc
+              prefix s f = Prefix (f <$ string s)
+
+
+factor = between (char '(') (char ')') expr
+         <|> (Var <$> many1 letter)
+         <|> (Num . read <$> many1 digit)
+         <?> "factor"
+
+juliaParse line = parse JuliaParser.expr "" (filter (/=' ') line)
