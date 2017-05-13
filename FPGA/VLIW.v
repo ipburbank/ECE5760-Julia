ebaf38efc4f03e96eff4e966d0a62ad9f597e5eb
Istvan Burbank <ipb7@cornell.edu>
2017-05-12 23:36:36 -0400

    Update VLIW to store RE{z} in reg0, Im{z} in reg1

--- FPGA/VLIW.v
+++ FPGA/VLIW.v
@@ -2,6 +2,7 @@
  * VLIW processor
  *
  * NOTES:
+ * - Re{z} in reg 0, Im{z} in reg 1
  * - behavior or saving to z from two pipes in the same cycle is undefined
  */
 
@@ -9,9 +10,11 @@ module VLIW (
              input wire        clk,
              input wire        reset,
              input wire        start,
-             input wire [9:0]  max_iterations,
              output reg        done,
+
+             input wire [9:0]  max_iterations,
              output wire [9:0] num_iterations,
+
              // instructions
              input wire        load_enable_input,
              input wire [26:0] load_value,
@@ -49,7 +52,8 @@ module VLIW (
    wire                        mul_enabled  = mul_enable_input;
 
    // Z REG
-   reg [26:0]                  z_reg; // value at address 0
+   reg [26:0]                  z_regRe; // value at address 0
+   reg [26:0]                  z_regIm; // value at address 1
 
    // SRC/DEST translation
    wire [7:0]                  load_dest_translated = load_dest_addr % 256;
@@ -57,24 +61,28 @@ module VLIW (
    wire [7:0]                  neg_src_translated   = neg_src_addr  % 256;
    wire [7:0]                  neg_dest_translated  = neg_dest_addr % 256;
    wire [26:0]                 ld_out_neg_src, neg_out_neg_src, add_out_neg_src, mul_out_neg_src;
-   wire [26:0]                 neg_src = (neg_src_translated == 0) ? z_reg      :
+   wire [26:0]                 neg_src = (neg_src_translated == 0) ? z_regRe    :
+                                         (neg_src_translated == 1) ? z_regIm    :
                                          (neg_src_addr < 256) ? ld_out_neg_src  :
                                          (neg_src_addr < 512) ? neg_out_neg_src :
                                          (neg_src_addr < 768) ? add_out_neg_src :
                                                                 mul_out_neg_src ;
    wire [26:0]                 neg_value_output;
 
+   reg [9:0]                   add_dest_addr_cycle2;
    wire [7:0]                  add_src1_translated  = add_src1_addr % 256;
    wire [7:0]                  add_src2_translated  = add_src2_addr % 256;
-   wire [7:0]                  add_dest_translated  = add_dest_addr % 256;
+   wire [7:0]                  add_dest_translated  = add_dest_addr_cycle2 % 256;
    wire [26:0]                 ld_out_add_src1, neg_out_add_src1, add_out_add_src1, mul_out_add_src1;
-   wire [26:0]                 add_src1 = (add_src1_translated == 0) ? z_reg       :
+   wire [26:0]                 add_src1 = (add_src1_translated == 0) ? z_regRe     :
+                                          (add_src1_translated == 1) ? z_regIm     :
                                           (add_src1_addr < 256) ? ld_out_add_src1  :
                                           (add_src1_addr < 512) ? neg_out_add_src1 :
                                           (add_src1_addr < 768) ? add_out_add_src1 :
                                                                   mul_out_add_src1 ;
    wire [26:0]                 ld_out_add_src2, neg_out_add_src2, add_out_add_src2, mul_out_add_src2;
-   wire [26:0]                 add_src2 = (add_src2_translated == 0) ? z_reg       :
+   wire [26:0]                 add_src2 = (add_src2_translated == 0) ? z_regRe     :
+                                          (add_src2_translated == 1) ? z_regIm     :
                                           (add_src2_addr < 256) ?  ld_out_add_src2 :
                                           (add_src2_addr < 512) ? neg_out_add_src2 :
                                           (add_src2_addr < 768) ? add_out_add_src2 :
@@ -85,13 +93,15 @@ module VLIW (
    wire [7:0]                  mul_src2_translated  = mul_src2_addr % 256;
    wire [7:0]                  mul_dest_translated  = mul_dest_addr % 256;
    wire [26:0]                 ld_out_mul_src1, neg_out_mul_src1, add_out_mul_src1, mul_out_mul_src1;
-   wire [26:0]                 mul_src1 = (mul_src1_translated == 0) ? z_reg       :
+   wire [26:0]                 mul_src1 = (mul_src1_translated == 0) ? z_regRe     :
+                                          (mul_src1_translated == 1) ? z_regIm     :
                                           (mul_src1_addr < 256) ? ld_out_mul_src1  :
                                           (mul_src1_addr < 512) ? neg_out_mul_src1 :
                                           (mul_src1_addr < 768) ? add_out_mul_src1 :
                                                                   mul_out_mul_src1 ;
    wire [26:0]                 ld_out_mul_src2, neg_out_mul_src2, add_out_mul_src2, mul_out_mul_src2;
-   wire [26:0]                 mul_src2 = (mul_src2_translated == 0) ? z_reg       :
+   wire [26:0]                 mul_src2 = (mul_src2_translated == 0) ? z_regRe     :
+                                          (mul_src2_translated == 1) ? z_regIm     :
                                           (mul_src2_addr < 256) ? ld_out_mul_src2  :
                                           (mul_src2_addr < 512) ? neg_out_mul_src2 :
                                           (mul_src2_addr < 768) ? add_out_mul_src2 :
@@ -103,19 +113,29 @@ module VLIW (
    //=======================================================
 
    always @(posedge clk) begin
-      if (reset) z_reg <= 0;
-      else if (load_enabled       && load_dest_translated == 0) z_reg <= load_value;
-      else if (neg_enabled        && neg_dest_translated  == 0) z_reg <= neg_value_output;
-      else if (add_enabled_cycle2 && add_dest_translated  == 0) z_reg <= add_value_output;
+      if (reset) begin
+         z_regRe <= 0;
+         z_regIm <= 0;
+      end
+      else if (load_enabled       && load_dest_translated == 0) z_regRe <= load_value;
+      else if (load_enabled       && load_dest_translated == 1) z_regIm <= load_value;
+      else if (neg_enabled        && neg_dest_translated  == 0) z_regRe <= neg_value_output;
+      else if (neg_enabled        && neg_dest_translated  == 1) z_regIm <= neg_value_output;
+      else if (add_enabled_cycle2 && add_dest_translated  == 0) z_regRe <= add_value_output;
+      else if (add_enabled_cycle2 && add_dest_translated  == 1) z_regIm <= add_value_output;
+      else if (mul_enabled        && mul_dest_translated  == 0) z_regRe <= mul_value_output;
+      else if (mul_enabled        && mul_dest_translated  == 1) z_regIm <= mul_value_output;
    end
 
    // state machine to run the add pipe
    always @(posedge clk) begin
       if(reset) begin
          add_enabled_cycle2 <= 0;
+         add_dest_addr_cycle2 <= 0;
       end
       else begin
          add_enabled_cycle2 <= add_enabled_cycle1;
+         add_dest_addr_cycle2 <= add_dest_addr;
       end
    end
 
