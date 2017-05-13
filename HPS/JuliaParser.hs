77fb394651a9e80efa4db5c91b86b2f360f6f044
Istvan Burbank <ipb7@cornell.edu>
2017-05-13 17:24:02 -0400

    Updated compiler to handle complex numbers
    In the form: aib

--- HPS/JuliaParser.hs
+++ HPS/JuliaParser.hs
@@ -10,6 +10,7 @@ import Data.Functor
 -- Taken From https://gist.github.com/m00nlight/2868363ec217f97072b4
 
 data Exp = Num Int
+         | Cpx Exp Exp -- complex
          | Var String
          | Add Exp Exp
          | Sub Exp Exp
@@ -25,6 +26,7 @@ expr = buildExpressionParser table factor <?> "expr"
 
 --table :: OperatorTable s u m Exp
 table = [[prefix "-" (Neg), prefix "+" (Pos)]
+        ,[op "i" (Cpx) AssocRight]
         ,[op "^" (Pow) AssocRight]
         ,[op "*" (Mul) AssocRight, op "/" (Div) AssocRight]
         ,[op "+" (Add) AssocLeft, op "-" (Sub) AssocLeft]]
